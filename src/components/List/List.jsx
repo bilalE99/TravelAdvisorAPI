ccb8537a425fe0a1ab9ff66dbc242675bb947b8b
bilalE99 <bilalelfitouri1999@gmail.com>
2022-02-03 23:15:02 +0000

    Set up the header, the list and it's select fields and also connecting the map API to allow it to be displayed.

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,10 +1,56 @@
-import React from 'react';
+import React, { useState} from 'react';
+import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, Select } from '@material-ui/core';
+
+import PlaceDetails from '../PlaceDetails/PlaceDetails';
+import useStyles from './styles';
+
+
 
 const List = () => {
-    return (
-        <h1>List</h1>
-    );
+    const classes = useStyles();
+    const [type, setType] = useState('restaurants');
+    const [rating, setRating] = useState('');
 
-}
+    const places = [
+        {name: 'Cool place 1'},      
+        {name: 'Cool place 2'},   
+        {name: 'Cool place 3'},   
+        {name: 'Cool place 5'},   
+        {name: 'Cool place 6'},   
+        {name: 'Cool place 7'},   
+        {name: 'Cool place 8'}  
+    ];
+    
+      return (
+    <div className={classes.container}>
+      <Typography variant="h4">Restaurants, Hotels & Attractions around you</Typography>
+          <FormControl className={classes.formControl}>
+            <InputLabel>Type</InputLabel>
+            <Select value={type} onChange={(e) => setType(e.target.value)}>
+              <MenuItem value="restaurants">Restaurants</MenuItem>
+              <MenuItem value="hotels">Hotels</MenuItem>
+              <MenuItem value="attractions">Attractions</MenuItem>
+            </Select>
+          </FormControl>
+          <FormControl className={classes.formControl}>
+            <InputLabel>Rating</InputLabel>
+            <Select value={rating} onChange={(e) => setRating(e.target.value)}>
+              <MenuItem value={0}>All</MenuItem>
+              <MenuItem value={3}>Above 3.0</MenuItem>
+              <MenuItem value={4}>Above 4.0</MenuItem>
+              <MenuItem value={4.5}>Above 4.5</MenuItem>
+            </Select>
+          </FormControl>
+          <Grid container spacing={3} className={classes.list}>
+            {/* only if you have 'places', only then you can map through the data */}
+            {places?.map((place, i) => (
+              <Grid key={i} item xs={12}>
+                <PlaceDetails place={place} />
+              </Grid>
+            ))}
+          </Grid>
+    </div>
+  );
+};
 
 export default List;
