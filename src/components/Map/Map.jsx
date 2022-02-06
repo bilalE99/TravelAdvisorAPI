0f08f12ba1e91ade871169e5756ce742ea69e3c7
bilalE99 <bilalelfitouri1999@gmail.com>
2022-02-06 04:28:38 +0000

    Set up the google maps connection by obtaining the users long+lat through the use of their current geolocation on the webrowser. Also set up the traveladvisor API to start to make requests for data based on location.

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,30 +1,37 @@
 import React from 'react';
 import GoogleMapReact from 'google-map-react';
-import {Papr, Typography, useMediaQuery} from '@material-ui/core';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
 import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
-import Rating from '@material-ui/lab';
+import Rating from '@material-ui/lab/Rating';
 
 import useStyles from './styles';
 
 
-const Map = () => {
+const Map = ({ coordinates, setCoordinates, setBounds }) => {
     const classes = useStyles();
     const isMobile = useMediaQuery('(min-width:600px)');
-    const coordinates = { lat: 0, lng: 0 };
 
     return (
-        <GoogleMapReact
-        bootstrapURLKeys={{ key: 'AIzaSyD5NodBRt-ikcLvSImUsgr0njZ0vbeKFrU' }}
-        defaultCenter={coordinates}
-        center={coordinates}
-        defaultZoom={14}
-        margin={[50, 50, 50, 50]}
-        options={''}
-        onChange={''}
-        onChildClick={''}
-        >
+        <div className={classes.mapContainer}>
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: 'AIzaSyD5NodBRt-ikcLvSImUsgr0njZ0vbeKFrU' }}
+                defaultCenter={coordinates}
+                center={coordinates}
+                defaultZoom={14}
+                margin={[50, 50, 50, 50]}
+                options={''}
+                onChange={(e) => {
+                    setCoordinates({ lat: e.center.lat, lng: e.center.lng });
+                    setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
+                    console.log(e);
+                }}
+                onChildClick={''}
 
-        </GoogleMapReact>
+            >
+
+            </GoogleMapReact>
+        </div>
+
     );
 
 }
